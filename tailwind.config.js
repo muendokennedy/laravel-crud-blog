2f46d2c72508054ae8f0dedd3eabeae6cfd7c69f
muendokennedy <kennedymunyao999@gmail.com>
2023-08-09 13:13:34 +0300

    Added a laravel breeze anthentication package

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './resources/views/**/*.blade.php',
+        './resources/views/**/*.js',
+        './resources/views/**/*.vue',
+    ],
+
+    theme: {
+        extend: {
+        },
+    },
+};
